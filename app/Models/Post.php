53c8b008eb82d28dacfd0d98ea52b0c35426b9f0
ahmed helalia <ahmedhelalia213@gmail.com>
2026-01-06 17:50:31 +0200

    managing posts & categories

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'thumbnail',
+        'title',
+        'color',
+        'slug',
+        'category_id',
+        'content',
+        'tags',
+        'published'
+    ];
+
+    protected $casts = [
+        'tags' => 'array',
+        'published' => 'boolean'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
